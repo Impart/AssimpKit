b6f6ad9e3c8ff8ec2b86d9b0817b1684f4595c5b
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2018-01-20 22:17:51 +0300

    - Fixed case when texture image files appended with new extension or when extension is missing

--- AssimpKit/Code/Model/SCNTextureInfo.m
+++ AssimpKit/Code/Model/SCNTextureInfo.m
@@ -277,12 +277,6 @@ static NSArray<NSURL *> *c_folders = nil;
         realImageUrl = [self getFilePathWithBaseURL:baseURL fileName:fixedFileName];
     }
     
-//    if (!realImageUrl) {
-//        // Sketchfab just appending .png to previous image name
-//        NSString *fixedFileName = [fileName stringByAppendingString:@".png"];
-//        realImageUrl = [self getFilePathWithBaseURL:baseURL fileName:fixedFileName];
-//    }
-    
     self.imageSource = CGImageSourceCreateWithURL((CFURLRef)realImageUrl, NULL);
     self.image = CGImageSourceCreateImageAtIndex(self.imageSource, 0, NULL);
     
@@ -343,7 +337,7 @@ static NSArray<NSURL *> *c_folders = nil;
             continue;
         }
         
-        if ([content.lastPathComponent.lowercaseString isEqualToString:fileName]) {
+        if ([content.lastPathComponent.lowercaseString hasPrefix:fileName]) {
             return content;
         }
     }
